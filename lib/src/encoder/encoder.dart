d15892768453b007a5c32d9b09e260c88c248c89
Ravi Teja Gudapati <tejainece@gmail.com>
2022-04-14 19:58:06 +0200

    encoder

---
+++ lib/src/encoder/encoder.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:characters/characters.dart';
+
+void encodeJSON(dynamic v, StreamSink<String> sink) {
+  if(v is num) {
+    sink.add(v.toString());
+  } else if(v == null) {
+    sink.add('null');
+  } else if(v is bool) {
+    sink.add(v.toString());
+  } else if(v is String) {
+    // TODO
+  } else if(v is Map<String, dynamic>) {
+    // TODO
+  } else if(v is List) {
+    // TODO
+  } else {
+    throw UnsupportedError('invalid type ${v.runtimeType}');
+  }
+}
+
+void _writeString(String v, StreamSink<String> sink) {
+  sink.add('"');
+  for(final c in v.characters) {
+    if(c == r'\') {
+        sink.add(r'\\');
+    } else if(c == r'/') {
+      sink.add(r'\/');
+    } else if(c == r'"') {
+      sink.add(r'\"');
+    } else if(c == '\b') {
+      sink.add(r'\b');
+    } else if(c == '\f') {
+      sink.add(r'\f');
+    } else if(c == '\r') {
+      sink.add(r'\r');
+    } else if(c == '\n') {
+      sink.add(r'\n');
+    } else if(c == '\t') {
+      sink.add(r'\t');
+    }
+    // TODO
+  }
+  sink.add('"');
+}
+
+void _writeMap(Map<String, dynamic> map, StreamSink<String> sink) {
+  sink.add('{');
+
+  for(final entry in map.entries) {
+
+  }
+
+  sink.add('}');
+}
